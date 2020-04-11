c46edba4b81c46a9715f1e23f8c9e494b6f5577f
Olexiy Talalay <49188915+TIIvnderg0D@users.noreply.github.com>
2020-04-12 00:38:24 +0300

    12.04

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import {ITEMS} from './Components/Buttons/items.js';
-import {Buttons, Status} from './Components/Buttons/index.js'
+import {Buttons} from './Components/Buttons/index.js'
+import { library } from './fontawesome.js'
 
 function App() {
   return (
