2d5c783321760c3df09806eabb6d4cd1cb38cca3
Olexiy Talalay <49188915+TIIvnderg0D@users.noreply.github.com>
2020-07-05 23:25:23 +0300

    05.07
    Transforming to HOC

--- src/Components/Buttons/index.js
+++ src/Components/Buttons/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './index.css' 
 import {Single} from './Single/Single.js'
 import {Multiple} from './Multiple/Multiple.js'
-import {Popover} from './Popover/Popover.js'
+import Popover from './Popover/Popover.js'
 
 export function Buttons({items}) {
     return (
       <div>
         <Single items={items}/>
         <Multiple items={items}/>
-        <Popover/>
+        <Popover />
       </div>
     );
   }
